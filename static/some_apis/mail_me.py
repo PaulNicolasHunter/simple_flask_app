c90ac9b5f379c34e92a63e9407293fa9ec7da6c9
PaulNicolasHunter <choudhary.vivek98@gmail.com>
2019-05-04 16:27:25 +0530

    it is done :') removed my email

--- static/some_apis/mail_me.py
+++ static/some_apis/mail_me.py
@@ -4,8 +4,8 @@ import smtplib
 def mail_me(user_mail, pro_mess):
 	mail_ref = smtplib.SMTP('smtp.gmail.com', 587)
 	mail_ref.starttls()
-	mail_ref.login("vivek16csu427@ncuindia.edu", "VivekChoudhary@2898")
+	mail_ref.login("<senders_email>", "<senders_password>")
 	message = pro_mess
 	# we can handle this by the use of cookies
-	mail_ref.sendmail("vivek16csu427@ncuindia.edu", user_mail, message)
+	mail_ref.sendmail("<senders_email>", user_mail, message)
 	mail_ref.quit()
